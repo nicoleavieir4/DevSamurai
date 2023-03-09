87357de91758c347e041fa3da9e2a17456362765
nicoleavieir4 <nicoleavieira@icloud.com>
2023-03-09 17:27:45 -0300

    while, do while && for

---
+++ exercicio-08/index.js
@@ -0,0 +1,27 @@
+//ESTRUTURAS DE REPETIÇÃO
+
+/* 
+DO...WHILE
+
+c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 5)
+
+*/
+
+/*
+
+WHILE
+var d = 1
+while(d <= 5){
+    console.log(`${d}o lugar.`)
+    d++
+}
+
+*/
+
+for (var c = 1; c <= 5; c++){
+    console.log(`Passo ${c}`)
+}
